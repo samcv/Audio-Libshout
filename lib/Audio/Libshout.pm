e18c21f443210f173860d3cb9b59afef9494df12
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-06-21 15:22:45 +0100

    Updated accessor-facade

--- lib/Audio/Libshout.pm
+++ lib/Audio/Libshout.pm
@@ -1,6 +1,6 @@
 use v6;
 use NativeCall;
-use AccessorFacade;
+use AccessorFacade:ver<v0.0.2>;
 
 class Audio::Libshout {
 
@@ -58,73 +58,73 @@ class Audio::Libshout {
         sub shout_set_host(Shout, Str) returns int32 is native('libshout') { * }
         sub shout_get_host(Shout) returns Str is native('libshout') { * }
 
-        method host() returns Str is rw is accessor_facade(&shout_get_host, &shout_set_host, &manage, &check) { }
+        method host() returns Str is rw is accessor-facade(&shout_get_host, &shout_set_host, &manage, &check) { }
 
         sub shout_set_port(Shout, int32) returns int32 is native('libshout') { * }
         sub shout_get_port(Shout) returns int32 is native('libshout') { * }
 
-        method port() returns Int is rw is accessor_facade(&shout_get_port, &shout_set_port, Code, &check) { }
+        method port() returns Int is rw is accessor-facade(&shout_get_port, &shout_set_port, Code, &check) { }
 
         sub shout_set_user(Shout, Str) returns int32 is native('libshout') { * }
         sub shout_get_user(Shout) returns Str is native('libshout') { * }
 
-        method user() returns Str is rw is accessor_facade(&shout_get_user, &shout_set_user, &manage, &check) { }
+        method user() returns Str is rw is accessor-facade(&shout_get_user, &shout_set_user, &manage, &check) { }
 
         sub shout_set_password(Shout, Str) returns int32 is native('libshout') { * }
         sub shout_get_password(Shout) returns Str is native('libshout') { * }
 
-        method password() returns Str is rw is accessor_facade(&shout_get_password, &shout_set_password, &manage, &check) { }
+        method password() returns Str is rw is accessor-facade(&shout_get_password, &shout_set_password, &manage, &check) { }
 
         sub shout_get_protocol(Shout) returns int32 is native('libshout') { * }
         sub shout_set_protocol(Shout, int32) returns int32 is native('libshout') { * }
 
-        method protocol() returns Protocol() is rw is accessor_facade(&shout_get_protocol, &shout_set_protocol, Code, &check) { }
+        method protocol() returns Protocol is rw is accessor-facade(&shout_get_protocol, &shout_set_protocol, Code, &check) { }
 
         sub shout_get_format(Shout) returns int32 is native('libshout') { * }
         sub shout_set_format(Shout, int32) returns int32 is native('libshout') { * }
 
-        method format() returns Format() is rw is accessor_facade(&shout_get_format, &shout_set_format, Code, &check) { }
+        method format() returns Format is rw is accessor-facade(&shout_get_format, &shout_set_format, Code, &check) { }
 
         sub shout_get_mount(Shout) returns Str is native('libshout') { * }
         sub shout_set_mount(Shout, Str) returns int32 is native('libshout') { * }
 
-        method mount() returns Str is rw is accessor_facade(&shout_get_mount, &shout_set_mount, &manage, &check) { }
+        method mount() returns Str is rw is accessor-facade(&shout_get_mount, &shout_set_mount, &manage, &check) { }
 
         sub shout_get_dumpfile(Shout) returns Str is native('libshout') { * }
         sub shout_set_dumpfile(Shout, Str ) returns int32 is native('libshout') { * }
 
-        method dumpfile() returns Str is rw is accessor_facade(&shout_get_dumpfile, &shout_set_dumpfile, &manage, &check ) { }
+        method dumpfile() returns Str is rw is accessor-facade(&shout_get_dumpfile, &shout_set_dumpfile, &manage, &check ) { }
 
         sub shout_get_agent(Shout) returns Str is native('libshout') { * }
         sub shout_set_agent(Shout, Str) returns int32 is native('libshout') { * }
 
-        method agent() returns Str is rw is accessor_facade(&shout_get_agent, &shout_set_agent, &manage, &check) { }
+        method agent() returns Str is rw is accessor-facade(&shout_get_agent, &shout_set_agent, &manage, &check) { }
 
         # Directory parameters
         sub shout_get_public(Shout) returns int32 is native('libshout') { * }
         sub shout_set_public(Shout, int32) returns int32 is native('libshout') { * }
 
-        method public returns Int is rw is accessor_facade(&shout_get_public, &shout_set_public, Code, &check) { }
+        method public returns Bool is rw is accessor-facade(&shout_get_public, &shout_set_public, Code, &check) { }
 
         sub shout_get_name(Shout) returns Str is native('libshout') { * }
         sub shout_set_name(Shout, Str) returns int32 is native('libshout') { * }
 
-        method name() returns Str is rw is accessor_facade(&shout_get_name, &shout_set_name, &manage, &check) { }
+        method name() returns Str is rw is accessor-facade(&shout_get_name, &shout_set_name, &manage, &check) { }
 
         sub shout_get_url(Shout) returns Str is native('libshout') { * }
         sub shout_set_url(Shout, Str) returns int32 is native('libshout') { * }
 
-        method url() returns Str is rw is accessor_facade(&shout_get_url, &shout_set_url, &manage, &check) { }
+        method url() returns Str is rw is accessor-facade(&shout_get_url, &shout_set_url, &manage, &check) { }
 
         sub shout_get_genre(Shout) returns Str is native('libshout') { * }
         sub shout_set_genre(Shout, Str) returns int32 is native('libshout') { * }
 
-        method genre() returns Str is rw is accessor_facade(&shout_get_genre, &shout_set_genre, &manage, &check) { }
+        method genre() returns Str is rw is accessor-facade(&shout_get_genre, &shout_set_genre, &manage, &check) { }
 
         sub shout_get_description(Shout) returns Str is native('libshout') { * }
         sub shout_set_description(Shout, Str) returns int32 is native('libshout') { * }
 
-        method description() returns Str is rw is accessor_facade(&shout_get_description, &shout_set_description, &manage, &check) { }
+        method description() returns Str is rw is accessor-facade(&shout_get_description, &shout_set_description, &manage, &check) { }
 
         # Not making a method for these quite yet.
         sub shout_get_audio_info(Shout, Str) returns Str is native('libshout') { * }
